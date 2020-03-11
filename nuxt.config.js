1ee94c799627bdcf2f73be632a5b1d8d7809f0c0
sumihiro3 <sumihiro@gmail.com>
2020-03-11 22:56:35 +0900

    add light color theme

--- nuxt.config.js
+++ nuxt.config.js
@@ -64,6 +64,15 @@ export default {
       dark: true,
       themes: {
         dark: {
+          primary: colors.lightBlue.darken4,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        },
+        light: {
           primary: colors.blue.darken2,
           accent: colors.grey.darken3,
           secondary: colors.amber.darken3,
